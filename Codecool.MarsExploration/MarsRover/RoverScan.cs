0fce094102b01d563a3734e14fec65c331b6c085
Tommer-04 <tothtom1205@gmail.com>
2024-04-24 14:51:41 +0200

    Rover scan algorithm

--- Codecool.MarsExploration/MarsRover/RoverScan.cs
+++ Codecool.MarsExploration/MarsRover/RoverScan.cs
@@ -13,7 +13,47 @@ namespace Codecool.MarsExploration.MarsRover
         public string[,] Scan(Coordinate currentPosition, Map map, int viewDistance)
         {
             int mapLength = map.Representation.GetLength(0);
+            int distanceDown;
+            int distanceUp;
+            int distanceLeft;
+            int distanceRight;
             string[,] scannedArea = new string[viewDistance * 2 + 1,viewDistance * 2 + 1];
+
+            //Calculate down
+            if(currentPosition.Y <= mapLength - 1 - viewDistance)
+            {
+                distanceDown = viewDistance;
+            } else
+            {
+                distanceDown = viewDistance - (currentPosition.Y - (mapLength - 1 - viewDistance));
+            }
+
+            //Calculate up
+            if(currentPosition.Y >= viewDistance) 
+            {
+                distanceUp = viewDistance;
+            } else
+            {
+                distanceUp = viewDistance + (currentPosition.Y - viewDistance);
+            }
+
+            //Calculate right
+            if(currentPosition.X <= mapLength - 1 - viewDistance)
+            {
+                distanceRight = viewDistance;
+            } else
+            {
+                distanceRight = viewDistance - (currentPosition.X - (mapLength - 1 - viewDistance));
+            }
+
+            //Calculate left
+            if(currentPosition.X >= viewDistance)
+            {
+                distanceLeft = viewDistance;
+            } else
+            {
+                distanceLeft =  viewDistance + (currentPosition.X - viewDistance);
+            }
             return default;
         }
     }
